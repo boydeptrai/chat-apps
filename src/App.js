f9d1e0655d0b4cb2c1e3410fca8748295ee369e1
boydeptrai <langthamht047@gmail.com>
2023-03-21 23:49:44 +0700

    chat app design

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.scss';
+import Home from './pages/Home';
+import Register from './pages/Register';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Register /> */}
+      <Home />
+    </div>
+  );
+}
+
+export default App;
